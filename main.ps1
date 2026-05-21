95e81d7fc4e06779ee5e545c048e5ed722714b1f
Богатов Алексей <70240595@online.muiv.ru>
2026-05-21 15:47:02 +0300

    Initial commit: создана базовая структура проекта

---
+++ main.ps1
@@ -0,0 +1,5 @@
+Write-Host "Система учёта компьютерной техники v1.0" -ForegroundColor Green
+Write-Host "1. Добавить оборудование"
+Write-Host "2. Показать всё оборудование"
+Write-Host "3. Поиск оборудования"
+Write-Host "4. Выход"
